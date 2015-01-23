6a05c73b7e4345c8cce48b0f497d524e1e091284
user <user@user-PC>
2015-01-23 18:44:29 +0900

    fdssf

--- demo/Scripts/main.js
+++ demo/Scripts/main.js
@@ -18,6 +18,7 @@ var Base = Class.extend({
 		this.frameCount = 0;
 	},
 	setPosition: function(x, y) {
+		//console.log("x="+x+",,y="+y);
 		this.x = x;
 		this.y = y;
 	},
@@ -139,7 +140,6 @@ var Matter = Base.extend({
  */
 var Ground = Matter.extend({
 	init: function(x, y, blocking, level) {
-		console.log(level+"--"+blocking);
 		this._super(x, y, blocking, level);
 	},
 });
@@ -276,7 +276,6 @@ var Figure = Base.extend({
 		this.level = level;
 		this._super(x, y);
 		level.figures.push(this);
-		console.log("Figure");
 	},
 	setState: function(state) {
 		this.state = state;
@@ -311,8 +310,8 @@ var Figure = Base.extend({
 		this._super(width, height);
 	},
 	setGridPosition: function(x, y) {
-		this.i = Math.floor((x + 16) / 32);
-		this.j = Math.ceil(this.level.getGridHeight() - 1 - y / 32);
+		this.i = Math.floor((x + 16) / 32); //32단위로 페이징처리 0~32=1, 32~64=2 ... 
+		this.j = Math.ceil(this.level.getGridHeight() - 1 - y / 32); //32단위로 페이징처리 0~32=14, 32~64=13 ...
 		
 		if(this.j > this.level.getGridHeight())
 			this.die();
@@ -323,7 +322,7 @@ var Figure = Base.extend({
 	setVelocity: function(vx, vy) {
 		this.vx = vx;
 		this.vy = vy;
-		
+		//console.log("this.vx값="+this.vx);
 		if(vx > 0)
 			this.direction = directions.right;
 		else if(vx < 0)
@@ -335,54 +334,63 @@ var Figure = Base.extend({
 	hit: function(opponent) {
 		
 	},
-	collides: function(is, ie, js, je, blocking) {
-		/*
+	collides: function(is, ie, js, je, blocking) { ///충돌체크
+		
 		var isHero = this instanceof Hero;
 		
-		if(is < 0 || ie >= this.level.obstacles.length)
+		if(is < 0 || ie >= this.level.obstacles.length) //his.level.obstacles.length=레벨width값만큼의 장애물
 			return true;
 			
 		if(js < 0 || je >= this.level.getGridHeight())
 			return false;
-			
+		//width값보다 크면 실행하지 않음	
+		console.log("is="+is+",,ie="+ie+",,js="+js+",,je="+je);	
 		for(var i = is; i <= ie; i++) {
 			for(var j = je; j >= js; j--) {
 				var obj = this.level.obstacles[i][j];
-				
 				if(obj) {
+					/*
 					if(obj instanceof Item && isHero && (blocking === ground_blocking.bottom || obj.blocking === ground_blocking.none))
 						obj.activate(this);
-					
+					*/
 					if((obj.blocking & blocking) === blocking)
 						return true;
+						
 				}
 			}
 		}
-		*/
+		
 		return false;
 	},
 	move: function() {
-		var vx = this.vx;
-		var vy = this.vy - constants.gravity;
+		var vx = this.vx; //x방향 속도
+		var vy = this.vy - constants.gravity; //y방향 속도
 		
-		var s = this.state;
+		var s = this.state; //
 		
-		var x = this.x;
-		var y = this.y;
+		var x = this.x; //x위치
+		var y = this.y; //y위치
 		
-		var dx = Math.sign(vx);
-		var dy = Math.sign(vy);
+		/*
+		math.sign = 부호조사
+		math.ceil = 수직선상 바로 오른쪽 정수
+		math.floor = 수직선상 바로 왼쪽정수
+		*/
+		var dx = Math.sign(vx); //방향확인 left=+값일때, right=-값일때
+		var dy = Math.sign(vy); //방향확인 bottom=+값일때, top=-값일때
 		
-		var is = this.i;
+		var is = this.i; // x방향 그리드
 		var ie = is;
 		
-		var js = Math.ceil(this.level.getGridHeight() - s - (y + 31) / 32);
-		var je = this.j;
+		var js = Math.ceil(this.level.getGridHeight() - s - (y + 31) / 32); //y뱡향 그리드와 s변수에 따라 1차이 또는 2차이
+		//s변수는 
+		var je = this.j; // y방향 그리드
 		
-		var d = 0, b = ground_blocking.none;
+		var d = 0, b = ground_blocking.none;//b=블락, d=x그리드 위치와, 몹의 그리드위치의 차이
 		var onground = false;
-		var t = Math.floor((x + 16 + vx) / 32);
-		
+		var t = Math.floor((x + 16 + vx) / 32); //vx 속도만큼의 몹의 그리드 위치
+	
+		//-------------------------------------------- x방향으로 움직임
 		if(dx > 0) {
 			d = t - ie;
 			t = ie;
@@ -391,9 +399,10 @@ var Figure = Base.extend({
 			d = is - t;
 			t = is;
 			b = ground_blocking.right;
+		
 		}
 		
-		x += vx;
+		x += vx; //vx값만큼 x위치를 더해줌 . 318+(-2),316+(-2)
 		
 		for(var i = 0; i < d; i++) {
 			if(this.collides(t + dx, t + dx, js, je, b)) {
@@ -405,8 +414,23 @@ var Figure = Base.extend({
 			t += dx;
 			is += dx;
 			ie += dx;
+			//console.log("t="+t+",,is="+is+",,ie="+ie);
 		}
 		
+		///// move 함수에서 사용하는 모든변수 확인
+		/*
+		console.log("-------------------");
+		console.log("vx="+vx+",,vy="+vy);
+		console.log("dx="+dx+",,dy="+dy);
+		console.log("x(위치값)="+x+",,y(위치값)="+y);
+		console.log("is="+is+",,ie="+ie);
+		console.log("js="+js+",,je="+je);
+		console.log("d="+d+",,b="+b);
+		console.log("t="+t+",,onground="+onground);
+		console.log("-------------------");
+		*/
+		
+		//-------------------------------------------- y방향으로 움직임
 		if(dy > 0) {
 			t = Math.ceil(this.level.getGridHeight() - s - (y + 31 + vy) / 32);
 			d = js - t;
@@ -432,7 +456,9 @@ var Figure = Base.extend({
 			
 			t -= dy;
 		}
-		
+		//------------------------------------------
+			
+
 		this.onground = onground;
 		this.setVelocity(vx, vy);
 		this.setPosition(x, y);
@@ -455,7 +481,6 @@ var Enemy = Figure.extend({
 	init: function(x, y, level) {
 		this._super(x, y, level);
 		this.speed = 0;
-		console.log("enemy");
 	},
 	hide: function() {
 		this.invisible = true;
@@ -498,18 +523,19 @@ var Enemy = Figure.extend({
 		this.die();
 	},
 	hit: function(opponent) {
-		if(this.invisible)
+		if(this.invisible) //죽었으면 hit 할필요 없음
 			return;
-		/*	
-		if(opponent instanceof Mario) {
+		
+		//console.log(opponent instanceof Mario);
+		if(opponent instanceof Mario) { ///마리오와 같다면
 			if(opponent.vy < 0 && opponent.y - opponent.vy >= this.y + this.state * 32) {
 				opponent.setVelocity(opponent.vx, constants.bounce);
-				this.hurt(opponent);
+				this.hurt(opponent); ///enemy는 죽는다
 			} else {
-				opponent.hurt(this);
+				opponent.hurt(this); //마리오는 죽는다.
 			}
 		}
-		*/
+		/*	*/
 	},
 });
 
@@ -520,12 +546,14 @@ var Enemy = Figure.extend({
  */
 var Gumpa = Enemy.extend({
 	init: function(x, y, level) {
+		console.log("gumpa");
+		
 		this._super(x, y, level);
 		this.setSize(34, 32);
 		this.setSpeed(constants.ballmonster_v);
 		this.death_mode = death_modes.normal;
 		this.deathCount = 0;
-		console.log("gumpa");
+	
 	},
 	setVelocity: function(vx, vy) {
 		this._super(vx, vy);
@@ -570,6 +598,264 @@ var Gumpa = Enemy.extend({
 		this._super();
 	},
 }, 'ballmonster');
+
+/*
+ * -------------------------------------------
+ * HERO CLASS
+ * -------------------------------------------
+ */
+var Hero = Figure.extend({
+	init: function(x, y, level) {
+		this._super(x, y, level);
+	},
+});
+
+/*
+ * -------------------------------------------
+ * MARIO CLASS
+ * -------------------------------------------
+ */
+var Mario = Hero.extend({
+	init: function(x, y, level) {
+		this.standSprites = [
+			[[{ x : 0, y : 81},{ x: 481, y : 83}],[{ x : 81, y : 0},{ x: 561, y : 83}]],
+			[[{ x : 0, y : 162},{ x: 481, y : 247}],[{ x : 81, y : 243},{ x: 561, y : 247}]]
+		];
+		this.crouchSprites = [
+			[{ x : 241, y : 0},{ x: 161, y : 0}],
+			[{ x : 241, y : 162},{ x: 241, y : 243}]
+		];
+		this.deadly = 0;
+		this.invulnerable = 0;
+		this.width = 80;
+		this._super(x, y, level);
+		this.blinking = 0;
+		this.setOffset(-24, 0);
+		this.setSize(80, 80);
+		this.cooldown = 0;
+		this.setMarioState(mario_states.normal);
+		this.setLifes(constants.start_lives);
+		this.setCoins(0);
+		this.deathBeginWait = Math.floor(700 / constants.interval);
+		this.deathEndWait = 0;
+		this.deathFrames = Math.floor(600 / constants.interval);
+		this.deathStepUp = Math.ceil(200 / this.deathFrames);
+		this.deathDir = 1;
+		this.deathCount = 0;
+		this.direction = directions.right;
+		this.setImage(images.sprites, 81, 0);
+		this.crouching = false;
+		this.fast = false;
+	},
+	setMarioState: function(state) {
+		this.marioState = state;
+	},
+	setState: function(state) {
+		if(state !== this.state) {
+			this.setMarioState(mario_states.normal);
+			this._super(state);
+		}
+	},
+	setPosition: function(x, y) {
+		this._super(x, y);
+		var r = this.level.width - 640;
+		var w = (this.x <= 210) ? 0 : ((this.x >= this.level.width - 230) ? r : r / (this.level.width - 440) * (this.x - 210));		
+		this.level.setParallax(w);
+
+		if(this.onground && this.x >= this.level.width - 128)
+			this.victory();
+	},
+	input: function(keys) {
+		this.fast = keys.accelerate;
+		this.crouching = keys.down;
+		
+		if(!this.crouching) {
+			if(this.onground && keys.up)
+				this.jump();
+				
+			if(keys.accelerate && this.marioState === mario_states.fire)
+				this.shoot();
+				
+			if(keys.right || keys.left)
+				this.walk(keys.left, keys.accelerate);
+			else
+				this.vx = 0;
+		}
+	},
+	victory: function() {
+		this.level.playMusic('success');
+		this.clearFrames();
+		this.view.show();
+		this.setImage(images.sprites, this.state === size_states.small ? 241 : 161, 81);
+		this.level.next();
+	},
+	shoot: function() {
+		if(!this.cooldown) {
+			this.cooldown = constants.cooldown;
+			this.level.playSound('shoot');
+			new Bullet(this);
+		}
+	},
+	setVelocity: function(vx, vy) {
+		if(this.crouching) {
+			vx = 0;
+			this.crouch();
+		} else {
+			if(this.onground && vx > 0)
+				this.walkRight();
+			else if(this.onground && vx < 0)
+				this.walkLeft();
+			else
+				this.stand();
+		}
+	
+		this._super(vx, vy);
+	},
+	blink: function(times) {
+		this.blinking = Math.max(2 * times * constants.blinkfactor, this.blinking || 0);
+	},
+	invincible: function() {
+		this.level.playMusic('invincibility');
+		this.deadly = Math.floor(constants.invincible / constants.interval);
+		this.invulnerable = this.deadly;
+		this.blink(Math.ceil(this.deadly / (2 * constants.blinkfactor)));
+	},
+	grow: function() {
+		if(this.state === size_states.small) {
+			this.level.playSound('grow');
+			this.setState(size_states.big);
+			this.blink(3);
+		}
+	},
+	shooter: function() {
+		if(this.state === size_states.small)
+			this.grow();
+		else
+			this.level.playSound('grow');
+			
+		this.setMarioState(mario_states.fire);
+	},
+	walk: function(reverse, fast) {
+		this.vx = constants.walking_v * (fast ? 2 : 1) * (reverse ? - 1 : 1);
+	},
+	walkRight: function() {
+		if(this.state === size_states.small) {
+			if(!this.setupFrames(8, 2, true, 'WalkRightSmall'))
+				this.setImage(images.sprites, 0, 0);
+		} else {
+			if(!this.setupFrames(9, 2, true, 'WalkRightBig'))
+				this.setImage(images.sprites, 0, 243);
+		}
+	},
+	walkLeft: function() {
+		if(this.state === size_states.small) {
+			if(!this.setupFrames(8, 2, false, 'WalkLeftSmall'))
+				this.setImage(images.sprites, 80, 81);
+		} else {
+			if(!this.setupFrames(9, 2, false, 'WalkLeftBig'))
+				this.setImage(images.sprites, 81, 162);
+		}
+	},
+	stand: function() {
+		var coords = this.standSprites[this.state - 1][this.direction === directions.left ? 0 : 1][this.onground ? 0 : 1];
+		this.setImage(images.sprites, coords.x, coords.y);
+		this.clearFrames();
+	},
+	crouch: function() {
+		var coords = this.crouchSprites[this.state - 1][this.direction === directions.left ? 0 : 1];
+		this.setImage(images.sprites, coords.x, coords.y);
+		this.clearFrames();
+	},
+	jump: function() {
+		this.level.playSound('jump');
+		this.vy = constants.jumping_v;
+	},
+	move: function() {
+		this.input(keys);		
+		this._super();
+	},
+	addCoin: function() {
+		this.setCoins(this.coins + 1);
+	},
+	playFrame: function() {		
+		if(this.blinking) {
+			if(this.blinking % constants.blinkfactor === 0)
+				this.view.toggle();
+				
+			this.blinking--;
+		}
+		
+		if(this.cooldown)
+			this.cooldown--;
+		
+		if(this.deadly)
+			this.deadly--;
+		
+		if(this.invulnerable)
+			this.invulnerable--;
+		
+		this._super();
+	},
+	setCoins: function(coins) {
+		this.coins = coins;
+		
+		if(this.coins >= constants.max_coins) {
+			this.addLife()
+			this.coins -= constants.max_coins;
+		}
+				
+		this.level.world.parent().children('#coinNumber').text(this.coins);
+	},
+	addLife: function() {
+		this.level.playSound('liveupgrade');
+		this.setLifes(this.lifes + 1);
+	},
+	setLifes : function(lifes) {
+		this.lifes = lifes;
+		this.level.world.parent().children('#liveNumber').text(this.lifes);
+	},
+	death: function() {
+		if(this.deathBeginWait) {
+			this.deathBeginWait--;
+			return true;
+		}
+		
+		if(this.deathEndWait)
+			return --this.deathEndWait;
+		
+		this.view.css({ 'bottom' : (this.deathDir > 0 ? '+' : '-') + '=' + (this.deathDir > 0 ? this.deathStepUp : this.deathStepDown) + 'px' });
+		this.deathCount += this.deathDir;
+		
+		if(this.deathCount === this.deathFrames)
+			this.deathDir = -1;
+		else if(this.deathCount === 0)
+			this.deathEndWait = Math.floor(1800 / constants.interval);
+			
+		return true;
+	},
+	die: function() {
+		this.setMarioState(mario_states.normal);
+		this.deathStepDown = Math.ceil(240 / this.deathFrames);
+		this.setupFrames(9, 2, false);
+		this.setImage(images.sprites, 81, 324);
+		this.level.playMusic('die');
+		this._super();
+	},
+	hurt: function(from) {
+		if(this.deadly)
+			from.die();
+		else if(this.invulnerable)
+			return;
+		else if(this.state === size_states.small) {
+			this.die();
+		} else {
+			this.invulnerable = Math.floor(constants.invulnerable / constants.interval);
+			this.blink(Math.ceil(this.invulnerable / (2 * constants.blinkfactor)));
+			this.setState(size_states.small);
+			this.level.playSound('hurt');			
+		}
+	},
+}, 'mario');
 
 
 /*
@@ -625,7 +911,7 @@ var Level = Base.extend({
 				
 				////150 line 에 grass_top 객체 생성
 				if(reflection[col[j]]){
-					//console.log("x="+i * 32+"y="+(height - j - 1) * 32);
+					//console.log(reflection[col[j]]+"--x="+i * 32+"y="+(height - j - 1) * 32);
 					new (reflection[col[j]])(i * 32, (height - j - 1) * 32, this);
 				}	
 				
@@ -645,7 +931,14 @@ var Level = Base.extend({
 
 		for(i = this.figures.length; i--; ) {
 			//console.log("i="+i);
-			figure = this.figures[i];
+			figure = this.figures[i];							
+			if(figure instanceof Mario) { ///마리오와 같다면
+				console.log("figure"+i+"=Mario");
+			}
+			if(figure instanceof Gumpa){
+				console.log("figure"+i+"=Gumpa");
+			}	
+			
 			if(figure.dead) {
 				if(!figure.death()) {
 					if(figure instanceof Mario)
@@ -656,20 +949,21 @@ var Level = Base.extend({
 				} else
 					figure.playFrame();
 			} else {
-				if(i) { //2개 이상일때부터 확인, 현재는 mario 객체는 i=0임
+					
+				if(i) { //2개 이상일때부터 확인
+								//1개는 부딪힐일이 없음.
 					for(j = i; j--; ) {
-						console.log("j="+j);
+						//console.log("j="+j);
+						
 						if(figure.dead)
 							break;
 						//figure=2일때 opponent=1,0	
 						opponent = this.figures[j];
+			
 						if(!opponent.dead && q2q(figure, opponent)) {
-						//q2q는 figure x,y 위치값이 opponent 위치값과 비교해서
-						//절반값 이상 겹치면 false를 내보냄
-						
-						//opponent가 살아있고, figure-opponent 가 겹치지 않으면
-						// 실행한다
-						
+							console.log("충돌함");
+						//q2q는 figure x,y 위치값이 opponent 위치값과 비교
+						//opponent가 살아있고, figure-opponent 가 겹치는 시점에 서로 부딪힌다			
 							figure.hit(opponent);
 							opponent.hit(figure);
 						}
@@ -681,6 +975,7 @@ var Level = Base.extend({
 				figure.move();
 				figure.playFrame();
 			}
+			console.log("----------------figure loop exit----------------");
 		}
 		/**/
 		for(i = this.items.length; i--; )
@@ -688,6 +983,9 @@ var Level = Base.extend({
 		
 		this.coinGauge.playFrame();
 		this.liveGauge.playFrame();
+		
+		
+			console.log("----------------tick exit----------------");
 	},
 	start: function() {
 		var me = this;
@@ -696,9 +994,11 @@ var Level = Base.extend({
 			me.tick.apply(me);
 		}, constants.interval);
 		*/
-		for(j = 25; j--; ) {
+		var j=30;
+		for(j = 3; j--; ) {
 			me.tick.apply(me);
 		}
+		
 	},
 	setPosition: function(x, y) {
 		this._super(x, y);
@@ -720,6 +1020,21 @@ var Level = Base.extend({
 	},
 	getGridHeight: function() {
 		return this.raw.height;
+	},
+	setParallax: function(x) {
+		this.setPosition(x, this.y);
+		this.world.parent().css('background-position', '-' + Math.floor(x / 3) + 'px -380px');
+	},
+	setSounds: function(manager) {
+		this.sounds = manager;
+	},
+	playSound: function(label) {
+		if(this.sounds)
+			this.sounds.play(label);
+	},
+	playMusic: function(label) {
+		if(this.sounds)
+			this.sounds.sideMusic(label);
 	},
 });
 
